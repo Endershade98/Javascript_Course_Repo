67b17389f6601727fdf50f502d12ed1e09adee4c
__EnderShade__ <decicconunzio@gmail.com>
2026-01-01 17:39:51 +0100

    Add JavaScript array examples in Chapter 3, demonstrating array creation and type checking

---
+++ Chapter_03_javascript_multiple_values/main.js
@@ -0,0 +1,13 @@
+arr1 = new Array("purple", "red", "green")
+arr2 = ["1", 2, "three", '4', 5.0]
+
+console.log("Array element 0:",arr1[0]);
+
+
+arr3 = new Array(10);
+arr4 = [10];
+
+console.log(typeof arr3);
+console.log(typeof arr3);
+console.log(typeof arr4);
+console.log(typeof arr4);
